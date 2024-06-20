e6fa26a2864123b6745c9d53fcf65378eb5fc96d
Aditya-Nagurkar <134510356+Aditya-Nagurkar@users.noreply.github.com>
2024-06-20 16:29:22 +0530

    Update dreampixel.py

--- dreampixel.py
+++ dreampixel.py
@@ -31,7 +31,7 @@ if submit_button:
             # Send a POST request to the model endpoint
             response = requests.post(
                 "https://api.stability.ai/v2beta/stable-image/generate/3",
-                headers={"Authorization": "Bearer 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"},
+                headers={"Authorization": "Bearer sk-uZZbuwhuvKKIUWDaXnu3DE89FtCsVg2Pib4Mj42jrLy2YErJ"},
                 json={"inputs": input_text}
             )
 
