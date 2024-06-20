ea131097b6022cd2555667ee3041e835d9dc50ac
Aditya-Nagurkar <134510356+Aditya-Nagurkar@users.noreply.github.com>
2024-06-20 19:05:29 +0530

    Update dreampixel.py

--- dreampixel.py
+++ dreampixel.py
@@ -30,7 +30,7 @@ if submit_button:
         with st.spinner("Generating image..."):
             # Send a POST request to the model endpoint
             response = requests.post(
-                "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-3-medium-diffusers",
+                "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
                 headers={"Authorization": "Bearer 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"},
                 json={"inputs": input_text}
             )
